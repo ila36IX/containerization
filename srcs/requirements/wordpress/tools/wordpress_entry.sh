cb60ad171fcd00cbaf0ee30c74efe194209b27b6
ila36ix <jbariali002@gmail.com>
2026-08-15 19:53:48 +0100

    Fix php permission when executing using nobody user

--- srcs/requirements/wordpress/tools/wordpress_entry.sh
+++ srcs/requirements/wordpress/tools/wordpress_entry.sh
@@ -5,37 +5,44 @@ MARIADB_USER_PASSWORD=$(cat "${MARIADB_USER_PASSWORD_FILE}")
 WP_ADMIN_PASSWORD=$(cat "${WP_ADMIN_PASSWORD_FILE}")
 WP_USER_PASSWORD=$(cat "${WP_USER_PASSWORD_FILE}")
 
-sed -i 's/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/g' /etc/php84/php-fpm.d/www.conf
+chown nobody:nobody /var/www/html
 
-echo "Waiting for MariaDB to be ready..."
-while ! mariadb-admin ping -h"${MARIADB_HOST}" -u"${MARIADB_USER_NAME}" -p"${MARIADB_USER_PASSWORD}" --silent; do
-	sleep 2
-done
+wp-cli() {
+	# NOTE: https://make.wordpress.org/cli/handbook/references/config/#environment-variables
+	WP_CLI_CACHE_DIR=/tmp/.wp-cli-cache \
+		su-exec nobody /usr/local/bin/wp --path="/var/www/html" "$@";
+}
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
+	while ! mariadb-admin ping -h"${MARIADB_HOST}" -u"${MARIADB_USER_NAME}" -p"${MARIADB_USER_PASSWORD}" --silent; do
+		sleep 2
+	done
 
-	wp --allow-root config create \
+	wp-cli  config create \
 		--dbname="${MARIADB_USER_DATABASE}" \
 		--dbuser="${MARIADB_USER_NAME}" \
 		--dbpass="${MARIADB_USER_PASSWORD}" \
-		--dbhost="${MARIADB_HOST}" \
-		--path="/var/www/html" \
-		
+		--dbhost="${MARIADB_HOST}";
 
-	wp --allow-root core install \
+
+	wp-cli core install \
 		--url="${WP_URL}" \
 		--title="${WP_TITLE}" \
 		--admin_user="${WP_ADMIN_USER}" \
 		--admin_password="${WP_ADMIN_PASSWORD}" \
-		--admin_email="${WP_ADMIN_EMAIL}" \
-		--path="/var/www/html" \
+		--admin_email="${WP_ADMIN_EMAIL}";
 
-	wp --allow-root user create \
+	wp-cli user create \
 		"${WP_USER}" \
 		"${WP_USER_EMAIL}" \
 		--user_pass="${WP_USER_PASSWORD}" \
-		--role=author \
-		--path="/var/www/html"
+		--role=author;
+
+	# Redis setup: https://github.com/rhubarbgroup/redis-cache/blob/develop/INSTALL.md
+	wp-cli config set WP_REDIS_HOST "redis" --path="/var/www/html"
+	wp-cli config set WP_REDIS_PORT "6379" --path="/var/www/html"
+	wp-cli plugin install redis-cache --path="/var/www/html" --activate
+	wp-cli redis enable --path="/var/www/html"
 fi
 
 exec /usr/sbin/php-fpm84 -F
