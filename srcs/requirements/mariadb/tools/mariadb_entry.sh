6e19bd8d5dd95344736e152ccb2aef7290160ccc
ila36ix <jbariali002@gmail.com>
2026-08-18 15:42:03 +0100

    Delete useless db user

--- srcs/requirements/mariadb/tools/mariadb_entry.sh
+++ srcs/requirements/mariadb/tools/mariadb_entry.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 set -e
 
-MARIADB_ROOT_PASSWORD=$(cat "${MARIADB_ROOT_PASSWORD_FILE:-/dev/null}")
-MARIADB_USER_PASSWORD=$(cat "${MARIADB_USER_PASSWORD_FILE:-/dev/null}")
-
-# NOTE: This can be useful in case of trying to use this dockerfile alone
-# MARIADB_USER_NAME="${MARIADB_USER_NAME:-user}"
-# MARIADB_USER_DATABASE="${MARIADB_USER_DATABASE:-userdb}"
-# MARIADB_ROOT_PASSWORD="${MARIADB_ROOT_PASSWORD:-root}"
-# MARIADB_USER_PASSWORD="${MARIADB_USER_PASSWORD:-user}"
+MARIADB_USER_PASSWORD=$(cat "${MARIADB_USER_PASSWORD_FILE}")
 
 echo "========================================"
 echo "DataBase variables:"
 echo "MARIADB_USER_NAME:          '${MARIADB_USER_NAME}'"
 echo "MARIADB_USER_DATABASE:      '${MARIADB_USER_DATABASE}'"
-echo "MARIADB_ROOT_PASSWORD:      '${MARIADB_ROOT_PASSWORD}'"
 echo "MARIADB_USER_PASSWORD:      '${MARIADB_USER_PASSWORD}'"
 echo "========================================"
 
@@ -26,7 +18,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
 	/usr/bin/mariadbd --user=mysql --bootstrap --skip-name-resolve << EOF
 FLUSH PRIVILEGES;
-GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}' WITH GRANT OPTION;
 CREATE DATABASE IF NOT EXISTS \`${MARIADB_USER_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MARIADB_USER_NAME}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';
 GRANT ALL ON \`${MARIADB_USER_DATABASE}\`.* TO '${MARIADB_USER_NAME}'@'%';
