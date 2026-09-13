b793adcf8ef58c811e147a5830769ea3f8cecda0
ila36ix <jbariali002@gmail.com>
2026-09-13 12:46:19 +0100

    Fix everything

--- srcs/requirements/bonus/ftp/tools/ftp_entry.sh
+++ srcs/requirements/bonus/ftp/tools/ftp_entry.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 set -e
 
-FTP_PASSWORD="$(cat $FTP_PASSWORD_FILE)"
+FTP_PASSWORD="$(cat /run/secrets/ftp_password)"
 
-if ! id "$FTP_USERNAME" >/dev/null 2>&1; then
-    adduser -h "$FTP_DIRECTORY" -s /sbin/nologin -D "$FTP_USERNAME"
+if ! id -u "$FTP_USERNAME" > /dev/null 2>&1; then
+    adduser -h $FTP_DIRECTORY -s /sbin/nologin -D $FTP_USERNAME
 fi
 
-echo "$FTP_USERNAME:$FTP_PASSWORD" | chpasswd
+echo $FTP_USERNAME:$FTP_PASSWORD | chpasswd
 
-usermod -d $FTP_DIRECTORY -s /sbin/nologin nobody
-
-chown -R nobody:nobody "$FTP_DIRECTORY"
-chmod 755 "$FTP_DIRECTORY"
+chown -R $FTP_USERNAME:$FTP_USERNAME $FTP_DIRECTORY
 
 mkdir -p /etc/vsftpd
-
-echo "$FTP_USERNAME" > /etc/vsftpd.user_list
 
 exec vsftpd "/etc/vsftpd/vsftpd.conf"
--- srcs/requirements/nginx/tools/nginx_entry.sh
+++ srcs/requirements/nginx/tools/nginx_entry.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-sed -i "s@WORDPRESS_PATH@$WORDPRESS_PATH@" /etc/nginx/http.d/default.conf
+sed -i "s@CONFIG_WORDPRESS_PATH@$WORDPRESS_PATH@" /etc/nginx/http.d/default.conf
+sed -i "s@CONFIG_DOMAIN_NAME@$DOMAIN_NAME@" /etc/nginx/http.d/default.conf
 
 exec nginx -g "daemon off;"
